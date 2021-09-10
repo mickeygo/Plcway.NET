e15dbae0b53b912cae58832c4785abe375d6b750
Yang Gang <gang.yang@mainzone.com.cn>
2021-09-11 00:31:02 +0800

    init

---
+++ src/Plcway.Infrastructure/Net/Transports/SocketPool.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Sockets;
+
+namespace Plcway.Infrastructure.Net.Transports
+{
+    /// <summary>
+    /// Socket 连接池
+    /// </summary>
+    public class SocketPool
+    {
+        public SocketPool()
+        {
+            
+        }
+
+        public Socket? Get()
+        {
+            return default;
+        }
+
+        public void Push()
+        {
+
+        }
+    }
+
+    internal class SocketEntry
+    {
+        public Socket Socket { get; set; }
+
+        public int Status { get; set; }
+
+        public long Version { get; set; }
+    }
+}
